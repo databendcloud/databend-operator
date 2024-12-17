ebd4019f780eb529e90150a174ada4f11f6c27d3
Electronic-Waste <2690692950@qq.com>
2024-12-17 05:47:45 +0000

    feat(doc): add swagger generation file.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

---
+++ hack/swagger/main.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/databendcloud/databend-operator/pkg/apis/databendlabs.io/v1alpha1"
+	"k8s.io/klog/v2"
+	"k8s.io/kube-openapi/pkg/common"
+	"k8s.io/kube-openapi/pkg/validation/spec"
+)
+
+// Generate OpenAPI spec definitions for Katib Resource
+func main() {
+	if len(os.Args) <= 2 {
+		klog.Fatal("Supply Swagger version and Katib Version")
+	}
+	version := os.Args[1]
+	if !strings.HasPrefix(version, "v") {
+		version = "v" + version
+	}
+	refCallback := func(name string) spec.Ref {
+		return spec.MustCreateRef("#/definitions/" + common.EscapeJsonPointer(swaggify(name)))
+	}
+
+	databendOperatorVersion := os.Args[2]
+	if databendOperatorVersion != "v1alpha1" {
+		klog.Fatalf("Databend Operator version %v is not supported", databendOperatorVersion)
+	}
+
+	oAPIDefs := v1alpha1.GetOpenAPIDefinitions(refCallback)
+	defs := spec.Definitions{}
+	for defName, val := range oAPIDefs {
+		defs[swaggify(defName)] = val.Schema
+	}
+	swagger := spec.Swagger{
+		SwaggerProps: spec.SwaggerProps{
+			Swagger:     "2.0",
+			Definitions: defs,
+			Paths:       &spec.Paths{Paths: map[string]spec.PathItem{}},
+			Info: &spec.Info{
+				InfoProps: spec.InfoProps{
+					Title:       "Databend Operator",
+					Description: "Swagger description for Databend Operator",
+					Version:     version,
+				},
+			},
+		},
+	}
+	jsonBytes, err := json.MarshalIndent(swagger, "", "  ")
+	if err != nil {
+		klog.Fatal(err.Error())
+	}
+	fmt.Println(string(jsonBytes))
+}
+
+func swaggify(name string) string {
+	name = strings.Replace(name, "github.com/databendcloud/databend-operator/pkg/apis/databendlabs.io/", "", -1)
+	name = strings.Replace(name, "/", ".", -1)
+	return name
+}
