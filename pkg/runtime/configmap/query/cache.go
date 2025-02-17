2ba1bab6a294d8e929c423297b2b565a9e5da0b7
Electronic-Waste <2690692950@qq.com>
2025-02-17 10:52:02 +0000

    fix(cache): rmeove some fields in CacheConfig.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

--- pkg/runtime/configmap/query/cache.go
+++ pkg/runtime/configmap/query/cache.go
@@ -6,11 +6,8 @@ import (
 )
 
 type CacheConfig struct {
-	DataCacheStorage               string           `toml:"data_cache_storage,omitempty"`
-	Disk                           *DiskCacheConfig `toml:"disk,omitempty"`
-	TableDataDeserializedDataBytes uint64           `toml:"table_data_deserialized_data_bytes,omitempty"`
-	TableBloomIndexFilterSize      uint64           `toml:"table_bloom_index_filter_size,omitempty"`
-	TableBloomIndexMetaCount       uint64           `toml:"table_bloom_index_meta_count,omitempty"`
+	DataCacheStorage string           `toml:"data_cache_storage,omitempty"`
+	Disk             *DiskCacheConfig `toml:"disk,omitempty"`
 }
 
 type DiskCacheConfig struct {
@@ -20,12 +17,6 @@ type DiskCacheConfig struct {
 
 func NewCacheConfig(tn *v1alpha1.Tenant, wh *v1alpha1.Warehouse) *CacheConfig {
 	cfg := &CacheConfig{}
-	cfg.TableBloomIndexFilterSize = 5368709120
-	cfg.TableBloomIndexMetaCount = 200000
-
-	// TODO(ws): ask about this configuration
-	// cfg.TableDataDeserializedDataBytes = resource.MaxTableMemoryCache
-
 	if wh == nil || tn == nil {
 		return cfg
 	}
