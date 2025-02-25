82efffd60d170a644e1bc54be7bf8bb78739d14c
Electronic-Waste <2690692950@qq.com>
2025-02-25 07:33:46 +0000

    chore(ingress): add builder for ingress.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

--- pkg/runtime/ingress/ingress.go
+++ pkg/runtime/ingress/ingress.go
@@ -4,6 +4,7 @@ import (
 	"github.com/databendcloud/databend-operator/pkg/apis/databendlabs.io/v1alpha1"
 	networkingv1 "k8s.io/api/networking/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 
 	"github.com/databendcloud/databend-operator/pkg/common"
 	"github.com/databendcloud/databend-operator/pkg/runtime/objectmeta"
@@ -17,6 +18,104 @@ func BuildIngress(tenant *v1alpha1.Tenant, wh *v1alpha1.Warehouse) *networkingv1
 			Labels:          objectmeta.LabelsFromWarehouse(wh),
 			Annotations:     BuildIngressAnnoations(wh),
 			OwnerReferences: objectmeta.BuildOwnerReferencesByWarehouse(wh),
+		},
+		Spec: networkingv1.IngressSpec{
+			IngressClassName: &wh.Spec.Ingress.IngressClassName,
+			Rules: []networkingv1.IngressRule{
+				{
+					Host: wh.Spec.Ingress.HostName,
+					IngressRuleValue: networkingv1.IngressRuleValue{
+						HTTP: &networkingv1.HTTPIngressRuleValue{
+							Paths: []networkingv1.HTTPIngressPath{
+								{
+									PathType: ptr.To(networkingv1.PathTypePrefix),
+									Path:    "/v1/admin",
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: common.GetQueryServiceName(tenant.Name, wh.Name),
+											Port: networkingv1.ServiceBackendPort{
+												Name: string(common.ServiceProtocolAdmin),
+											},
+										},
+									},
+								},
+								{
+									PathType: ptr.To(networkingv1.PathTypePrefix),
+									Path:    "/v1/ckhttp",
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: common.GetQueryServiceName(tenant.Name, wh.Name),
+											Port: networkingv1.ServiceBackendPort{
+												Name: string(common.ServiceProtocolCKHttp),
+											},
+										},
+									},
+								},
+								{
+									PathType: ptr.To(networkingv1.PathTypePrefix),
+									Path:    "/v1/flight",
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: common.GetQueryServiceName(tenant.Name, wh.Name),
+											Port: networkingv1.ServiceBackendPort{
+												Name: string(common.ServiceProtocolFlight),
+											},
+										},
+									},
+								},
+								{
+									PathType: ptr.To(networkingv1.PathTypePrefix),
+									Path:    "/v1/flightsql",
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: common.GetQueryServiceName(tenant.Name, wh.Name),
+											Port: networkingv1.ServiceBackendPort{
+												Name: string(common.ServiceProtocolFlightSQL),
+											},
+										},
+									},
+								},
+								{
+									PathType: ptr.To(networkingv1.PathTypePrefix),
+									Path:    "/v1/metrics",
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: common.GetQueryServiceName(tenant.Name, wh.Name),
+											Port: networkingv1.ServiceBackendPort{
+												Name: string(common.ServiceProtocolMetrics),
+											},
+										},
+									},
+								},
+								{
+									PathType: ptr.To(networkingv1.PathTypePrefix),
+									Path:    "/v1/mysql",
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: common.GetQueryServiceName(tenant.Name, wh.Name),
+											Port: networkingv1.ServiceBackendPort{
+												Name: string(common.ServiceProtocolMySQL),
+											},
+										},
+									},
+								},
+								{
+									PathType: ptr.To(networkingv1.PathTypePrefix),
+									Path:    "/v1/query",
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: common.GetQueryServiceName(tenant.Name, wh.Name),
+											Port: networkingv1.ServiceBackendPort{
+												Name: string(common.ServiceProtocolQuery),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 }
