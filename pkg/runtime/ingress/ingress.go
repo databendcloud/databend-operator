72865d3e180859014ed3357fa5bcfcdfb6d14fc5
Electronic-Waste <2690692950@qq.com>
2025-02-25 07:11:53 +0000

    chore(ingress): build annotations for ingress.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

--- pkg/runtime/ingress/ingress.go
+++ pkg/runtime/ingress/ingress.go
@@ -12,9 +12,10 @@ import (
 func BuildIngress(tenant *v1alpha1.Tenant, wh *v1alpha1.Warehouse) *networkingv1.Ingress {
 	return &networkingv1.Ingress{
 		ObjectMeta: v1.ObjectMeta{
-			Name:      common.GetQueryIngressName(tenant.Name, wh.Name),
-			Namespace: wh.Namespace,
-			Labels:    objectmeta.LabelsFromWarehouse(wh),
+			Name:            common.GetQueryIngressName(tenant.Name, wh.Name),
+			Namespace:       wh.Namespace,
+			Labels:          objectmeta.LabelsFromWarehouse(wh),
+			Annotations:     BuildIngressAnnoations(wh),
 			OwnerReferences: objectmeta.BuildOwnerReferencesByWarehouse(wh),
 		},
 	}
@@ -22,12 +23,20 @@ func BuildIngress(tenant *v1alpha1.Tenant, wh *v1alpha1.Warehouse) *networkingv1
 
 func BuildIngressAnnoations(wh *v1alpha1.Warehouse) map[string]string {
 	annotations := map[string]string{
-    	"external-dns.alpha.kubernetes.io/cloudflare-proxied": "false",
-    	"external-dns.alpha.kubernetes.io/hostname": wh.Spec.Ingress.HostName,
-    	"external-dns.alpha.kubernetes.io/ingress-hostname-source": "annotation-only",
-    	"external-dns.alpha.kubernetes.io/ttl": "100",
-    	"nginx.ingress.kubernetes.io/proxy-body-size": "10240m",
-    	"nginx.ingress.kubernetes.io/proxy-send-timeout": "3600s",
+		"external-dns.alpha.kubernetes.io/cloudflare-proxied":      "false",
+		"external-dns.alpha.kubernetes.io/hostname":                wh.Spec.Ingress.HostName,
+		"external-dns.alpha.kubernetes.io/ingress-hostname-source": "annotation-only",
+		"external-dns.alpha.kubernetes.io/ttl":                     "100",
+		"nginx.ingress.kubernetes.io/proxy-body-size":              "10240m",
+		"nginx.ingress.kubernetes.io/proxy-send-timeout":           "3600s",
+	}
+
+	for k, v := range wh.Spec.Ingress.Annotations {
+		annotations[k] = v
+	}
+
+	if wh.Spec.Ingress.EnableLoadBalance {
+		annotations["nginx.ingress.kubernetes.io/upstream-hash-by"] = "$http_x_databend_route_hint"
 	}
 
 	return annotations
