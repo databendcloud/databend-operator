08b4ccc4d7ddfc2a931ec172b219fdf74a7769b6
Electronic-Waste <2690692950@qq.com>
2025-03-07 03:11:30 +0000

    chore(ci): add k3d config file.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

--- scripts/gha/setup-k3d-cluster.sh
+++ scripts/gha/setup-k3d-cluster.sh
@@ -1,2 +1,51 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Configure variables.
+NAMESPACE="databend-system"
+TIMEOUT="5m"
+
+
+
+echo "Update databend operator manifest with newly built image"
+cd manifests
+kustomize edit set image datafuselabs/databend-operator=${OPERATOR_CI_IMAGE}
+
+echo "Installing databend operator manifests"
+kustomize build . | kubectl apply -f -
+
+(kubectl wait deployment/databend-operator --for=condition=available  -n ${NAMESPACE} --timeout ${TIMEOUT} &&
+  kubectl wait pods --for=condition=ready -n ${NAMESPACE} --timeout ${TIMEOUT} --all) ||
+  (
+    echo "Failed to wait until databend operator is ready" &&
+      kubectl get pods -n ${NAMESPACE} &&
+      kubectl describe pods -n ${NAMESPACE} &&
+      exit 1
+  )
+
+print_cluster_info() {
+  kubectl version
+  kubectl cluster-info
+  kubectl get nodes
+  kubectl get pods -n ${NAMESPACE}
+  kubectl describe pod -n ${NAMESPACE}
+}
+
+print_cluster_info
+
+echo "Deploying meta cluster with helm"
+helm repo add databend https://charts.databend.com
+helm install meta databend/databend-meta --namespace ${NAMESPACE}
+
+(kubectl wait pods --for=condition=ready -n ${NAMESPACE} --timeout ${TIMEOUT} --all) ||
+  (
+    echo "Failed to wait until meta cluster is ready" &&
+      kubectl get pods -n ${NAMESPACE} &&
+      kubectl describe pods -n ${NAMESPACE} &&
+      exit 1
+  )
+
+print_cluster_info
